330e9b8d278b398d33c43128526ecd5d51cb7d8a
djantz <dcjantz@gmail.com>
2026-07-18 09:35:35 -0700

    Update error handling in URL routing
    - Defined custom error handlers (`400`, `403`, `404`, `500`) in `urls.py`.
    - Replaced default error views with custom views from `app.views`.
    - Adjusted debug error routes to use the new custom error handlers.

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -2,9 +2,13 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from django.views import defaults as default_views
 
 admin.site.site_url = r"/enterpriseviz"
+
+handler400 = "app.views.error_400_view"
+handler403 = "app.views.error_403_view"
+handler404 = "app.views.error_404_view"
+handler500 = "app.views.error_500_view"
 
 urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
@@ -17,23 +21,13 @@ urlpatterns = [
 if settings.DEBUG:
     # This allows the error pages to be debugged during development, just visit
     # these url in browser to see how these error pages look like.
+    from app import views as app_views
+
     urlpatterns += [
-        path(
-            "400/",
-            default_views.bad_request,
-            kwargs={"exception": Exception("Bad Request!")},
-        ),
-        path(
-            "403/",
-            default_views.permission_denied,
-            kwargs={"exception": Exception("Permission Denied")},
-        ),
-        path(
-            "404/",
-            default_views.page_not_found,
-            kwargs={"exception": Exception("Page not Found")},
-        ),
-        path("500/", default_views.server_error),
+        path("400/", app_views.error_400_view),
+        path("403/", app_views.error_403_view),
+        path("404/", app_views.error_404_view),
+        path("500/", app_views.error_500_view),
     ]
     if "debug_toolbar" in settings.INSTALLED_APPS:
         import debug_toolbar
